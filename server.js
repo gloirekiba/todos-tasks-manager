2250f1b453f5ec4ff11a64e0d34273acf2c1d3fd
gloirekiba <gloirekiba21@gmail.com>
2022-07-16 19:15:22 +0200

    🗃️ add database connection

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
+// 1h:11
+
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
 // Import the necessary modules
 const express = require("express");
+const mongoose = require("mongoose");
 
 // Impport routes
 const taskRouter = require("./routes/tasks");
@@ -17,7 +20,16 @@ app.use(express.json());
 // Set routes
 app.use("/api/v1/tasks", taskRouter);
 
-// Initialize server
-app.listen(process.env.PORT, () => {
-  console.log(`App listening on port ${process.env.PORT}`);
+// Connect to the database
+mongoose.connect(process.env.MONGODB_URI);
+const db = mongoose.connection;
+
+db.on("error", (err) => console.error(err));
+db.once("open", () => {
+  console.log("Connected to MongoDB");
+
+  // Initialize server
+  app.listen(process.env.PORT, () => {
+    console.log(`App listening on port ${process.env.PORT}`);
+  });
 });
